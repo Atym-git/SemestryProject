456c0ac882323181d1701020c2aca104a327e1ce
Atym-git <chuvachelaad@gmail.com>
2025-01-27 14:58:25 +0300

    Main Menu

---
+++ SemProj/Assets/Scripts/ThroughScenes.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ThroughScenes : MonoBehaviour
+{
+
+    public void GoToGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
